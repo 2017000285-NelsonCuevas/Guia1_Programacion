4720cc5fdf4dd97f11a68458e0af75aaac704536
2017000285-NelsonCuevas <n.cuevas.5adb@gmail.com>
2026-04-18 16:20:21 -0600

    Add files via upload

---
+++ Act3_Lec20_Inc2.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingresa una nota: ");
+        int nota = Convert.ToInt32(Console.ReadLine());
+
+        if (nota >= 70)
+            Console.WriteLine("ALUMNO APROBADO");
+        else
+            Console.WriteLine("ALUMNO REPROBADO");
+    }
+}
