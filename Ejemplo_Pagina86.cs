4eff57e1187c258d8458e792c439dcf6c73a9f91
2017000285-NelsonCuevas <n.cuevas.5adb@gmail.com>
2026-04-18 16:13:00 -0600

    Add files via upload

---
+++ Ejemplo_Pagina86.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingresa un numero: ");
+        float num = Convert.ToInt32(Console.ReadLine());
+
+        if (num < 0)
+            Console.WriteLine("El numero es negativo: ");
+        else
+            Console.WriteLine("El numero es positivo: ");
+    }
+}
