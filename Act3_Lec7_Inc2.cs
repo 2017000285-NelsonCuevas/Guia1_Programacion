ec36baf95387360fe43b70ffd6d4d207e2353de8
2017000285-NelsonCuevas <n.cuevas.5adb@gmail.com>
2026-04-18 16:16:43 -0600

    Add files via upload

---
+++ Act3_Lec7_Inc2.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingresa el lado 1: ");
+        int lado1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingresa el lado 2: ");
+        int lado2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingresa el lado 3: ");
+        int lado3 = Convert.ToInt32(Console.ReadLine());
+
+        if ((lado1 == lado2) && (lado2 == lado3))
+            Console.WriteLine("El triangulo es: Equilatero");
+        else if ((lado1 == lado2) || (lado1 == lado3) || (lado2 == lado3))
+            Console.WriteLine("El triangulo es: Isosceles");
+        else
+            Console.WriteLine("El triangulo es: Escaleno");
+    }
+}
