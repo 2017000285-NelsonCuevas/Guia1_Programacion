2f9ad9dd36fbbcf9119662f2cc3d1eedbc14e231
2017000285-NelsonCuevas <n.cuevas.5adb@gmail.com>
2026-04-18 16:31:29 -0600

    Add files via upload

---
+++ Act1_Lec7_Inc2.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.Design;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Digite una edad: ");
+        int edad = Convert.ToInt32(Console.ReadLine());
+
+        if (edad >= 18)
+            Console.WriteLine("ES MAYOR DE EDAD");
+        else
+            Console.WriteLine("ES MENOR DE EDAD");
+    }
+}
