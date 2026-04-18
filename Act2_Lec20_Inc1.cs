96d90bf4a76a9be3d002b75f5d2cba8bc7e27d11
2017000285-NelsonCuevas <n.cuevas.5adb@gmail.com>
2026-04-18 16:29:53 -0600

    Add files via upload

---
+++ Act2_Lec20_Inc1.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingresa tu contraseña: ");
+        string num;
+        num = Console.ReadLine();
+
+        if (num == "Password123")
+            Console.WriteLine("!!!BIENVENIDO¡¡¡");
+        else
+            Console.WriteLine("EL INGRESO ESTA PROHIBIDO");
+    }
+}
