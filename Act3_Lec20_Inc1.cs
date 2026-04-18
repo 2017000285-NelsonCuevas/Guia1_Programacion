c7f6773b6a3ce9ddcf7113df0aeaf91e901c3d8b
2017000285-NelsonCuevas <n.cuevas.5adb@gmail.com>
2026-04-18 16:21:54 -0600

    Add files via upload

---
+++ Act3_Lec20_Inc1.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingresa un numero de la semana (1-7):");
+        int dia = Convert.ToInt32(Console.ReadLine());
+
+        if ((dia == 6) || (dia == 7))
+            Console.WriteLine("FIN DE SEMANA");
+        else if ((dia >= 1) && (dia <= 5))
+            Console.WriteLine("ENTRE SEMANA");
+        else
+            Console.WriteLine("NUMERO INVALIDO");
+
+    }
+}
